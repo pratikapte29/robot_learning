5c4a435f24f1539fc612920adfb8f445c310c777
Pratik <pratikapte2911@gmail.com>
2026-04-27 23:29:22 +0200

    Implement 1.3 with epsilon greedy selection strategy

--- experiments.py
+++ experiments.py
@@ -8,7 +8,7 @@ def select_action_uniformly():
 
 
 def run_five_armed_bandit(bandit, n_actions: int) -> float:
-    """_summary_
+    """Implementing 1.2
 
     Args:
         bandit : instance of the class FiveArmedBandits
@@ -23,3 +23,45 @@ def run_five_armed_bandit(bandit, n_actions: int) -> float:
         sum_reward += reward
 
     return sum_reward / n_actions
+
+
+def run_epsilon_greedy(bandit, n_actions: int, epsilon=0.1):
+    """Implementing 1.3
+
+    Args:
+        bandit (_type_): instance of the class FiveArmedBandits
+        n_actions (int): number of actions to simulate
+        epsilon (float, optional): probability of choosing a random action.
+          Defaults to 0.1.
+    """
+
+    Q = [0, 0, 0, 0, 0]  # initial value estimates of each arm
+    N = [0, 0, 0, 0, 0]  # store how many times each arm has been selected
+
+    sum_reward = 0
+
+    for i in range(n_actions):
+        if np.random.rand() < epsilon:
+            action = np.random.randint(0, 5)
+        else:
+            action = np.argmax(Q)
+
+        N[action] += 1
+
+        reward = bandit.sample_reward(action)
+        sum_reward += reward
+
+        Q[action] += (reward - Q[action]) / N[action]
+
+
+        # logging for every 100 actions
+
+        if (i+1) % 100 == 0:
+            step = i + 1
+            print(f"\nStep {step}")
+
+            print("Action % distribution:")
+            for j in range(5):
+                print(f"Arm {j+1}: {(N[j] / step) * 100:.2f}%")
+
+            print(f"Avg reward: {sum_reward / step:.4f}")
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from bandit_class import FiveArmedBandit
 from experiments import run_five_armed_bandit
+from experiments import run_epsilon_greedy
 
 
 bandit_inst = FiveArmedBandit()
 sample_average_reward = run_five_armed_bandit(bandit_inst, 10)
 
 print("The sample average reward is: ", sample_average_reward)
+
+run_epsilon_greedy(bandit=bandit_inst, n_actions=5000, epsilon=1)
